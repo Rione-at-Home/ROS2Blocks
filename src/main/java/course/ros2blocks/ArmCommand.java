bd066231962050657a324483633541afd48ef9b4
Great GIlbert Soco <gilsocojp@gmail.com>
2026-07-13 19:48:28 +0900

    Added ArmCommand class and renamed BaseMoveCommand class to BaseCommand

---
+++ src/main/java/course/ros2blocks/ArmCommand.java
@@ -0,0 +1,19 @@
+package course.ros2blocks;
+
+public class ArmCommand extends RobotCommand {
+    private final String method;
+
+    public ArmCommand(String method) {
+        this.method = method;
+
+    }
+
+    @Override
+    public String getPythonCommand() {
+        return String.format("self.robot.arm.%s()", method);
+
+    }
+
+    @Override
+    public String getDisplayName() { return method; }
+}
