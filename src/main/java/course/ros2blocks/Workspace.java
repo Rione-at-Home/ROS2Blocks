2b60372f3a461cdc51fe23061ec6d89e1ed534e3
Great GIlbert Soco <gilsocojp@gmail.com>
2026-07-13 23:07:44 +0900

    Add drag-and-drop ability in Workspace

--- src/main/java/course/ros2blocks/Workspace.java
+++ src/main/java/course/ros2blocks/Workspace.java
@@ -1,5 +1,6 @@
 package course.ros2blocks;
 
+import javafx.scene.input.TransferMode;
 import javafx.scene.layout.VBox;
 import java.util.ArrayList;
 import java.util.List;
@@ -7,11 +8,30 @@ import java.util.List;
 public class Workspace extends VBox {
     private final List<RobotCommand> script = new ArrayList<>();
 
+    public Workspace() {
+        this.setStyle("-fx-border-color: gray; -fx-border-width: 2;");
+
+        this.setOnDragOver(event -> {
+            if (event.getGestureSource() != this) {
+                event.acceptTransferModes(TransferMode.MOVE);
+            }
+            event.consume();
+        });
+
+        this.setOnDragDropped(event -> {
+            // Logic to instantiate the block based on the dropped type
+            // Add to workspace list
+            event.setDropCompleted(true);
+            event.consume();
+        });
+    }
+
     public void addCommand(RobotCommand cmd) {
         script.add(cmd);
         this.getChildren().add(new DraggableBlock(cmd));
 
     }
+
 
     public String exportToPython() {
         StringBuilder sb = new StringBuilder();
